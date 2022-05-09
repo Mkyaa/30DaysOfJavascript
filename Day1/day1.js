98b08da3319c0ee4660942e26c031844f009b3a6
Mkyaa <kayamert@live.com>
2022-05-09 17:36:08 +0300

    Day1
    I'm learning Javascript in 30 days

---
+++ Day1/day1.js
@@ -0,0 +1,13 @@
+console.log('İlk js kodum')
+
+// This is first command
+
+/* This is second command */
+
+console.log(3%2)
+
+// Comments can make code readable
+// Welcoe to 30DaysofJavascript
+/* Comments can make code readable,
+easy to ruse and informative*/
+
